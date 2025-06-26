9a046817ff0ec15323f395404a0f0f8780d8dde1
Jpmaster-creator <joaopedrodsn20@gmail.com>
2025-06-26 11:03:20 -0300

    NIVEL NOVATO
    Adicionado os comentários necessários.

--- xadrez.c
+++ xadrez.c
@@ -9,33 +9,36 @@ int main()
     int rainha = 1;
     int bispo = 1;
 
-
-
+    // Imprime o nome da peça do xadrex.
     printf("\n==========TORRE==========\n");
 
+    // Inicia o loop da peça torre, utilizando o comando for.
     for (int torre = 1; torre <= 5; torre++)
     {
         printf("\nDireita\n");
     }
-    
-    
+
+    // Imprime o nome da peça do xadrex.
     printf("\n==========RAINHA==========\n");
 
+    // Inicia o loop da peça Rainha, utilizando o comando while.
     while (rainha <= 8)
     {
         printf("\nEsquerda\n");
+        // Incrimenta 1 para evitar loop infinito.
         rainha++;
     }
 
+    // Imprime o nome da peça bispo.
     printf("\n==========BISPO==========\n");
 
+    // Inicia o loop da peça bispo, utilizando o comando do-while.
     do
     {
         printf("\nCima, Direita\n");
+        // Incrimenta 1 para evitar loop infinito.
         bispo++;
     } while (bispo <= 5);
-    
-
 
     return 0;
 }
